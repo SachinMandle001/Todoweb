94f669b3599dcc35ad0505797abbfdfa91f8373b
SachinMandle001 <sachinmandle2001@gmail.com>
2025-07-05 20:48:56 +0530

    save build app

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 require('dotenv').config()
 const express=require('express')
 const app =express()
+const path=require('path')
 
 const userRouter =require('./routes/route')
 app.use(express.json())
 
+app.use(express.static(path.join(__dirname,'dist')))
+
 const { connectDB } = require('./database/mydbconnection')
+
+app.get('/',(req, res)=>{
+    res.sendFile(path.join(__dirname,'dist/index.html'))
+})
 
 app.use('/api',userRouter)
 
